394c7ca0ad823547901557702212b43ebb3fc625
Rishav <rishav.m.19.02@gmail.com>
2021-03-29 19:33:19 +0530

    Minor changes

--- test/src/Api.js
+++ test/src/Api.js
@@ -1,4 +1,4 @@
-const key = '86e262cd5cb45b74a8084d2fe42eafbc'; /* Your API key here */
+const key = ''; /* Your API key here */
 const api = `https://api.themoviedb.org/3/movie/popular?api_key=${key}`;
 
 export default api;

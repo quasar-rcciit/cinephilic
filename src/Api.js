b518829d556876a12391e4f6f557b96499151048
Krystal Jean Austin <44277065+KrystalJean@users.noreply.github.com>
2021-10-28 14:33:43 -0500

    built frontend search bar

--- src/Api.js
+++ src/Api.js
@@ -1,4 +1,6 @@
 const key = ''; /* Assign your API KEYS here */
 const api = `https://api.themoviedb.org/3/movie/popular?api_key=${key}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${key}`;
 
 export default api;
+export default SEARCH_API;
